1ac602d1af5529d3f9df8c9eb9930279bc26923d
Taichi03 <Taichi03@users.noreply.github.com>
2020-09-01 18:25:48 +0900

    ユーザー登録機能実装

--- db/migrate/20200829152722_devise_create_users.rb
+++ db/migrate/20200829152722_devise_create_users.rb
@@ -5,13 +5,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
-      t.string :lust_name,          null: false
+      t.string :last_name,          null: false
       t.string :first_name,         null: false
-      t.string :lust_name_kana,     null: false
+      t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.date :birth_day,            null: false
+      t.date :birth_date,           null: false
 
       ## Recoverable
       t.string   :reset_password_token

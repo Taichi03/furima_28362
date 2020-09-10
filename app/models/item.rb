c0ee6326a85948328330033493150e007989da5d
Taichi03 <Taichi03@users.noreply.github.com>
2020-09-10 15:48:43 +0900

    テストエラー修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to_active_hash :item_category
   belongs_to_active_hash :item_prefecture
   belongs_to_active_hash :item_sales_status
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
-  factory :users do
+  factory :user do
     nickname {'test'}
     email {'test@test'}
     password {'test12'}
     first_name {'田中'}
     last_name {'太郎'}
-    first_name_kana {'タナカ'}
-    last_name_kana {'タロウ'}
+    first_name_kana {'たなか'}
+    last_name_kana {'たろう'}
     birth_date {'1930'}
   end
 end    
